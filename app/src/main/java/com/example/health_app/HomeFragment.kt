ea8d70f59d9f7866a3a6c0850a32aceba17f45bc
jung0302 <seungjung32@gmail.com>
2022-10-12 10:43:25 +0900

    프레그안에 프레그 추가

--- app/src/main/java/com/example/health_app/HomeFragment.kt
+++ app/src/main/java/com/example/health_app/HomeFragment.kt
@@ -1,13 +1,46 @@
 package com.example.health_app
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationRequest
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.net.URI.create
 
 
 class HomeFragment : Fragment() {
+
+
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
+    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+    internal lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
+    private val REQUEST_PERMISSION_LOCATION = 10
+
+    lateinit var button: Button
+    lateinit var text1: TextView
+    lateinit var text2: TextView
+    lateinit var geocoder: Geocoder
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +53,20 @@ class HomeFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+
+        button = view.findViewById(R.id.button)
+        text1 = view.findViewById(R.id.text1)
+        text2 = view.findViewById(R.id.text2)
+
+
+
+
         return view
     }
 
 }
+
+
 
 
 
