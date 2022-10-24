0ef55106480cb14a2f656ca66e7ede7da475c869
guswns98 <rkd90609@naver.com>
2022-10-24 22:46:19 +0900

    Top, Bottom, Aerobic - Activity 설명 추가 (6항목 까지만)

--- app/src/main/java/com/example/health_app/TopList.kt
+++ app/src/main/java/com/example/health_app/TopList.kt
@@ -15,7 +15,7 @@ class TopList : AppCompatActivity() {
 
         listView = findViewById(R.id.listView)
         val listView = findViewById<ListView>(R.id.listView)
-        val item = arrayOf("벤치 프레스","숄더 프레스","비하인드넥","랫풀다운","푸쉬업","케이블푸쉬다운","바벨 로우","덤벨 로우","펜들레이 로우","시티드 로우 머신","친업","하이퍼 익스텐션","바벨 풀오버","인클라인 바벨 로우")
+        val item = arrayOf("벤치프레스","바벨 로우","덤벨 로우","펜들레이 로우","랫풀다운","친업","하이퍼 익스텐션","오버헤드 프레스","덤벨 숄더 프레스","덤벨 레터럴 레이즈","덤벨 프론트 레이즈","덤벨 슈러그","비하인드 넥 프레스","바벨 컬 ")
         listView.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,item)
 
         listView.setOnItemClickListener { adapterView, view, position, l ->
